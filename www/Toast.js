44fdc2ba5c230b75c81e3c88a9ca8489ac36b693
Mansur <988302229@sms.tj>
2020-07-18 14:28:50 +0500

    fix

--- www/Toast.js
+++ www/Toast.js
@@ -1,5 +1,24 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'Toast', 'coolMethod', [arg0]);
+// exports.coolMethod = function (arg0, success, error) {
+//     exec(success, error, 'Toast', 'coolMethod', [arg0]);
+// };
+var hlToast = function() {
+    console.log('AndroidToast instanced');
 };
+
+hlToast.prototype.show = function(msg, onSuccess, onError) {
+    var errorCallback = function(obj) {
+        onError(obj);
+    };
+
+    var successCallback = function(obj) {
+        onSuccess(obj);
+    };
+
+    exec(successCallback, errorCallback, 'hlToast', 'show', [msg]);
+};
+
+if (typeof module != 'undefined' && module.exports) {
+    module.exports = AndroidToast;
+}
